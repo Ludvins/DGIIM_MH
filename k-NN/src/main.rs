f1efb97f8810cc4783f9cc8602d09063ca760bcf
Ludvins <theludvins@gmail.com>
2019-03-16 19:51:19 +0100

    Reorganize code

--- k-NN/src/main.rs
+++ k-NN/src/main.rs
@@ -28,7 +28,45 @@ fn make_partitions<T: Data<T> + Clone + Copy>(data: &Vec<T>, folds: usize) -> Ve
     return partitions;
 }
 
-pub fn _1nn_test<T: Data<T> + Clone + Copy>(knowledge: &Vec<T>, exam: &Vec<T>) -> i32 {
+pub fn classifier_1nn_weights<T: Data<T> + Clone + Copy>(
+    knowledge: &Vec<T>,
+    exam: &Vec<T>,
+    weights: &Vec<f32>,
+    discard_low_weights: bool,
+) -> i32 {
+    let mut correct = 0;
+    for result in exam.iter() {
+        let mut nearest_example: T = T::new();
+        let mut min_distance: f32 = std::f32::MAX;
+
+        for known in knowledge.iter() {
+            //NOTE Distance with weights
+            let mut distance = 0.0;
+            for index in 0..weights.len() {
+                // NOTE weights below 0.2 aren't considered.
+                if !discard_low_weights || weights[index] >= 0.2 {
+                    distance += weights[index]
+                        * (result.get_attr(index) - known.get_attr(index))
+                        * (result.get_attr(index) - known.get_attr(index))
+                }
+            }
+
+            distance = distance.sqrt();
+
+            if distance < min_distance {
+                min_distance = distance;
+                nearest_example = known.clone();
+            }
+        }
+
+        if nearest_example.get_class() == result.get_class() {
+            correct += 1;
+        }
+    }
+    return correct;
+}
+
+pub fn classifier_1nn<T: Data<T> + Clone + Copy>(knowledge: &Vec<T>, exam: &Vec<T>) -> () {
     let mut _correct: i32 = 0;
     for item in exam.iter() {
         let mut nearest_example: T = T::new();
@@ -36,7 +74,6 @@ pub fn _1nn_test<T: Data<T> + Clone + Copy>(knowledge: &Vec<T>, exam: &Vec<T>) -
 
         for known in knowledge.iter() {
             let distance = item.euclidean_distance(known);
-
             if distance < min_distance {
                 min_distance = distance;
                 nearest_example = known.clone();
@@ -49,187 +86,104 @@ pub fn _1nn_test<T: Data<T> + Clone + Copy>(knowledge: &Vec<T>, exam: &Vec<T>) -
     }
 
     println!(
-        "\t\tTest results: {}/{} = {}",
+        "\t\t1-NN Results: {}/{} = {}",
         _correct,
         exam.len(),
         _correct as f32 / exam.len() as f32
     );
-
-    return _correct;
 }
 
 pub fn relief<T: Data<T> + Clone + Copy>(
-    data: &Vec<Vec<T>>,
-    folds: usize,
-    _n_attrs: usize,
-    total_attempts: f32,
+    knowledge: &Vec<T>,
+    exam: &Vec<T>,
+    n_attrs: usize,
     discard_low_weights: bool,
 ) -> Result<(), Box<std::error::Error>> {
-    let mut total_correct = 0;
-    let mut _weights: Vec<f32> = vec![0.0; _n_attrs];
-
     // NOTE For each partition
-    for i in 0..folds {
-        let mut _correct = 0;
-        let mut _attempts = 0;
-        let test = data[i].clone();
+    let mut weights: Vec<f32> = vec![0.0; n_attrs];
 
-        // NOTE Test over partition i
-        let mut knowledge: Vec<T> = Vec::new();
+    // NOTE Greedy
+    for known in knowledge.iter() {
+        let mut enemy_distance = std::f32::MAX;
+        let mut friend_distance = std::f32::MAX;
+        let mut ally_index = 0;
+        let mut enemy_index = 0;
 
-        // Learning from all other partitions.
-        for j in 0..folds {
-            if j != i {
-                knowledge.extend(data[j].iter().cloned());
+        for (index, candidate) in knowledge.iter().enumerate() {
+            //NOTE Skip if cantidate == known
+            if candidate.get_id() != known.get_id() {
+                // NOTE Pre-calculate distance
+                let dist = known.euclidean_distance(candidate);
+                // NOTE Ally
+                if known.get_class() == candidate.get_class() {
+                    if dist < friend_distance {
+                        ally_index = index;
+                        friend_distance = dist;
+                    }
+                }
+                // NOTE Enemy
+                else {
+                    if dist < enemy_distance {
+                        enemy_index = index;
+                        enemy_distance = dist;
+                    }
+                }
             }
         }
+        let enemy: T = knowledge[enemy_index].clone();
+        let ally: T = knowledge[ally_index].clone();
 
-        // NOTE Greedy
-        for known in knowledge.iter() {
-            let mut enemy_distance = std::f32::MAX;
-            let mut friend_distance = std::f32::MAX;
-            let mut ally_index = 0;
-            let mut enemy_index = 0;
-
-            for (index, candidate) in knowledge.iter().enumerate() {
-                //NOTE Skip if cantidate == known
-                if candidate.get_id() != known.get_id() {
-                    // NOTE Pre-calculate distance
-                    let dist = known.euclidean_distance(candidate);
-                    // NOTE Ally
-                    if known.get_class() == candidate.get_class() {
-                        if dist < friend_distance {
-                            ally_index = index;
-                            friend_distance = dist;
-                        }
-                    }
-                    // NOTE Enemy
-                    else {
-                        if dist < enemy_distance {
-                            enemy_index = index;
-                            enemy_distance = dist;
-                        }
-                    }
-                }
-            }
-
-            let enemy: T = knowledge[enemy_index].clone();
-            let ally: T = knowledge[ally_index].clone();
-
-            // NOTE Re-calculate weights
-            let mut highest_weight: f32 = _weights[0];
-            for attr in 0.._n_attrs {
-                _weights[attr] += f32::abs(known.get_attr(attr) - enemy.get_attr(attr))
-                    - f32::abs(known.get_attr(attr) - ally.get_attr(attr));
-
-                // NOTE Find maximal element for future normalizing.
-                if _weights[attr] > highest_weight {
-                    highest_weight = _weights[attr];
-                }
-                // NOTE Truncate negative values as 0.
-                if _weights[attr] < 0.0 {
-                    _weights[attr] = 0.0;
-                }
-            }
-
-            // NOTE Normalize weights
-            for attr in 0.._n_attrs {
-                _weights[attr] = _weights[attr] / highest_weight;
-            }
-        } // NOTE END Greedy
-
-        // NOTE Test
-        for result in test.iter() {
-            _attempts += 1;
-
-            let mut nearest_example: T = T::new();
-            let mut min_distance: f32 = std::f32::MAX;
-
-            for known in knowledge.iter() {
-                //NOTE Distance with weights
-                let mut distance = 0.0;
-                for index in 0.._n_attrs {
-                    // NOTE weights below 0.2 aren't considered.
-                    if discard_low_weights || _weights[index] >= 0.2 {
-                        distance += _weights[index]
-                            * (result.get_attr(index) - known.get_attr(index))
-                            * (result.get_attr(index) - known.get_attr(index))
-                    }
-                }
-
-                distance = distance.sqrt();
-
-                if distance < min_distance {
-                    min_distance = distance;
-                    nearest_example = known.clone();
-                }
-            }
-
-            if nearest_example.get_class() == result.get_class() {
-                _correct += 1;
-            }
+        // NOTE Re-calculate weights
+        for attr in 0..n_attrs {
+            weights[attr] += (known.get_attr(attr) - enemy.get_attr(attr)).abs()
+                - (known.get_attr(attr) - ally.get_attr(attr)).abs();
         }
+    } // NOTE END Greedy
 
-        total_correct += _correct;
-        println!(
-            "\t\tTest results {}: {}/{} = {}",
-            i,
-            _correct,
-            _attempts,
-            _correct as f32 / _attempts as f32
-        );
+    let mut highest_weight: f32 = 0.0;
+    for attr in 0..n_attrs {
+        // NOTE Find maximal element for future normalizing.
+        if weights[attr] > highest_weight {
+            highest_weight = weights[attr];
+        }
+        // NOTE Truncate negative values as 0.
+        if weights[attr] < 0.0 {
+            weights[attr] = 0.0;
+        }
     }
 
     let mut reduction: f32 = 0.0;
-
-    for w in _weights {
-        if w < 0.2 {
+    // NOTE Normalize weights
+    for attr in 0..n_attrs {
+        weights[attr] = weights[attr] / highest_weight;
+        if weights[attr] < 0.2 {
             reduction += 1.0;
         }
     }
-    reduction = 100.0 * reduction / _n_attrs as f32;
 
-    let f = 0.5 * reduction + 0.5 * (100.0 * total_correct as f32 / total_attempts);
+    let correct = classifier_1nn_weights(&knowledge, &exam, &weights, discard_low_weights);
 
-    println!(
-        "\t\tReduction rate: {} \n\t\tSuccess percentage: {}/{} = {}\n\t\tEvaluation function: {}",
-        reduction,
-        total_correct,
-        total_attempts,
-        100.0 * total_correct as f32 / total_attempts,
-        f
-    );
+    reduction = 100.0 * reduction / n_attrs as f32;
 
-    Ok(())
-}
+    let f = 0.5 * reduction + 0.5 * (100.0 * correct as f32 / exam.len() as f32);
 
-pub fn _1_nn_loop<T: Data<T> + Clone + Copy>(
-    data: &Vec<Vec<T>>,
-    _n_attrs: usize,
-    _folds: usize,
-    _total: f32,
-) -> Result<(), Box<std::error::Error>> {
-    let mut _correct = 0;
-    for i in 0.._folds {
-        // NOTE Test over partition i
-
-        let mut knowledge: Vec<T> = Vec::new();
-
-        // Learning from all other partitions.
-        for j in 0.._folds {
-            if j != i {
-                knowledge.extend(&data[j]);
-            }
-        }
-
-        _correct += _1nn_test(&knowledge, &data[i]);
+    if discard_low_weights {
+        println!(
+            "\t\tRelief discarding weights under 0.2: \n\t\t\tReduction rate: {} \n\t\t\tSuccess percentage: {}/{} = {}\n\t\t\tEvaluation function: {}",
+            reduction,
+            correct,
+            exam.len(),
+            100.0 * correct as f32 / exam.len() as f32,
+            f);
+    } else {
+        println!(
+            "\t\tRelief without discarding weights under 0.2: \n\t\t\tReduction rate: {} \n\t\t\tSuccess percentage: {}/{} = {}\n\t\t\tEvaluation function: {}",
+            reduction,
+            correct,
+            exam.len(),
+            100.0 * correct as f32 / exam.len() as f32,
+            f);
     }
-    println!(
-        "\t\tFinal results: {}/{} = {}",
-        _correct,
-        550,
-        _correct as f32 / _total
-    );
 
     Ok(())
 }
@@ -264,26 +218,31 @@ fn run<T: Data<T> + Clone + Copy>(
 
         data.push(aux_record);
     }
-    let size = data.len() as f32;
 
     let data: Vec<Vec<T>> = make_partitions(&data, 5);
 
-    //NOTE 1-NN
-    let mut now = Instant::now();
-    println!("\tResuls for 1-NN:");
-    _1_nn_loop(&data, _n_attrs, _folds, size)?;
-    println!("\t Time elapsed: {} ms.\n", now.elapsed().as_millis());
-    //NOTE Relief descartando
-    now = Instant::now();
-    println!("\tResults for Relief discarding weights under 0.2:");
-    relief(&data, _folds, _n_attrs, size, true)?;
-    println!("\t Time Elapsed: {} ms.\n", now.elapsed().as_millis());
+    for i in 0.._folds {
+        let mut knowledge: Vec<T> = Vec::new();
+        for j in 0.._folds {
+            if j != i {
+                knowledge.extend(&data[j]);
+            }
+        }
+        println!("\tPartition test: {}", i);
+        let exam = data[i].clone();
 
-    //NOTE Relief sin descartar
-    now = Instant::now();
-    println!("\tResults for Relief without discarding weights under 0.2:");
-    relief(&data, _folds, _n_attrs, size, false)?;
-    println!("\t Time elapsed: {} ms.\n", now.elapsed().as_millis());
+        let mut now = Instant::now();
+        classifier_1nn(&knowledge, &exam);
+        println!("\t\t Time elapsed: {} ms.\n", now.elapsed().as_millis());
+
+        now = Instant::now();
+        relief(&knowledge, &exam, _n_attrs, true)?;
+        println!("\t\t Time Elapsed: {} ms.\n", now.elapsed().as_millis());
+
+        now = Instant::now();
+        relief(&knowledge, &exam, _n_attrs, false)?;
+        println!("\t\t Time elapsed: {} ms.\n", now.elapsed().as_millis());
+    }
 
     Ok(())
 }
