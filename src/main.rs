9429181f2ac632ad7c3d2ba9a2b2adf1b496f63d
Ludvins <theludvins@gmail.com>
2019-03-18 18:08:07 +0100

    Add local search and Results struct

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,50 @@
 extern crate csv;
+extern crate rand;
 extern crate serde_derive;
 
 //use std::sync::{Arc, Mutex}; //TODO Concurrency
 //use std::thread;
 
 pub mod structs;
-
+use rand::distributions::{Distribution, Normal, Uniform};
+use rand::seq::SliceRandom;
+use rand::thread_rng;
 use std::collections::HashMap;
 use std::time::Instant;
 use structs::*;
+
+fn refresh_index_vec(index_vec: &mut Vec<usize>, size: usize) -> usize {
+    let mut rng = thread_rng();
+    index_vec.clear();
+    for index in 0..size {
+        index_vec.push(index);
+    }
+    index_vec.shuffle(&mut rng);
+
+    let ret = index_vec[0];
+    index_vec.remove(0);
+    return ret;
+}
+
+fn normalize_and_truncate_weights(weights: &mut Vec<f32>) {
+    // NOTE Normalize
+    let mut highest_weight: f32 = 0.0;
+    for attr in 0..weights.len() {
+        // NOTE Find maximal element for future normalizing.
+        if weights[attr] > highest_weight {
+            highest_weight = weights[attr];
+        }
+        // NOTE Truncate negative values as 0.
+        if weights[attr] < 0.0 {
+            weights[attr] = 0.0;
+        }
+    }
+
+    // NOTE Normalize weights
+    for attr in 0..weights.len() {
+        weights[attr] = weights[attr] / highest_weight;
+    }
+}
 
 /// Makes partitions keeping the class diversity.
 ///
@@ -19,7 +55,6 @@ use structs::*;
 ///
 /// # Returns
 /// Returns a vector with `folds`  vectos of `T`.
-
 pub fn make_partitions<T: Data<T> + Clone + Copy>(data: &Vec<T>, folds: usize) -> Vec<Vec<T>> {
     let mut categories_count = HashMap::new();
     let mut partitions: Vec<Vec<T>> = Vec::new();
@@ -37,6 +72,42 @@ pub fn make_partitions<T: Data<T> + Clone + Copy>(data: &Vec<T>, folds: usize) -
 
     return partitions;
 }
+/// Clasifies all items in `exam`, using the ones in `knowledge`.
+///
+/// # Arguments
+/// * `knowledge` - Items whose class is known.
+/// * `exam` - Items whose class is unknown.
+///
+/// # Returns
+///
+/// Returns aninstance of Results.
+///
+/// **Note**: This classifier uses no weights, all attributes have the same value.
+
+pub fn classifier_1nn<T: Data<T> + Clone + Copy>(knowledge: &Vec<T>, exam: &Vec<T>) -> Results {
+    let mut correct: u32 = 0;
+    // NOTE Make the test over each element in exam.
+    for test in exam.iter() {
+        let mut nearest_example: T = T::new();
+        let mut min_distance: f32 = std::f32::MAX;
+
+        for known in knowledge.iter() {
+            let distance = test.euclidean_distance(known);
+            if distance < min_distance {
+                min_distance = distance;
+                nearest_example = known.clone();
+            }
+        }
+
+        // NOTE Verify if test was clasified correctly.
+        if nearest_example.get_class() == test.get_class() {
+            correct += 1;
+        }
+    }
+
+    let results = Results::new(0, correct, exam.len(), 0);
+    return results;
+}
 
 /// Clasifies all items in `exam`, using the ones in `knowledge` and weights for each attribute.
 ///
@@ -48,24 +119,24 @@ pub fn make_partitions<T: Data<T> + Clone + Copy>(data: &Vec<T>, folds: usize) -
 ///
 /// # Returns
 ///
-/// Returns a `i32` with the number of correct items classified.
+/// Returns an instance of Results.
 
 pub fn classifier_1nn_weights<T: Data<T> + Clone + Copy>(
     knowledge: &Vec<T>,
     exam: &Vec<T>,
     weights: &Vec<f32>,
     discard_low_weights: bool,
-) -> i32 {
-    let mut correct = 0;
+) -> Results {
+    let mut correct: u32 = 0;
+    // NOTE Make the test over each element in exam.
     for test in exam.iter() {
         let mut nearest_example: T = T::new();
         let mut min_distance: f32 = std::f32::MAX;
 
         for known in knowledge.iter() {
-            //NOTE Distance with weights
             let mut distance = 0.0;
             for index in 0..weights.len() {
-                // NOTE weights below 0.2 aren't considered.
+                // NOTE Weights below 0.2 aren't considered if `discarding_low_weights`.
                 if !discard_low_weights || weights[index] >= 0.2 {
                     distance += weights[index]
                         * (test.get_attr(index) - known.get_attr(index))
@@ -81,49 +152,20 @@ pub fn classifier_1nn_weights<T: Data<T> + Clone + Copy>(
             }
         }
 
+        // NOTE Verify it `test` was clasified correctly.
         if nearest_example.get_class() == test.get_class() {
             correct += 1;
         }
     }
-    return correct;
-}
-/// Clasifies all items in `exam`, using the ones in `knowledge`.
-///
-/// # Arguments
-/// * `knowledge` - Items whose class is known.
-/// * `exam` - Items whose class is unknown.
-///
-/// # Returns
-///
-/// Returns a `i32` with the number of correct items classified.
-///
-/// **Note**: This classifier uses no weights, all attributes have the same value.
-
-pub fn classifier_1nn<T: Data<T> + Clone + Copy>(knowledge: &Vec<T>, exam: &Vec<T>) -> () {
-    let mut _correct: i32 = 0;
-    for test in exam.iter() {
-        let mut nearest_example: T = T::new();
-        let mut min_distance: f32 = std::f32::MAX;
-
-        for known in knowledge.iter() {
-            let distance = test.euclidean_distance(known);
-            if distance < min_distance {
-                min_distance = distance;
-                nearest_example = known.clone();
-            }
-        }
-
-        if nearest_example.get_class() == test.get_class() {
-            _correct += 1;
+    let mut low = 0;
+    for attr in 0..weights.len() {
+        if weights[attr] < 0.2 {
+            low += 1;
         }
     }
 
-    println!(
-        "\t\t1-NN Results: {}/{} = {}",
-        _correct,
-        exam.len(),
-        _correct as f32 / exam.len() as f32
-    );
+    let results = Results::new(low, correct, exam.len(), weights.len());
+    return results;
 }
 /// Prepares weights using a Greedy algorithm, so they are used in `classifier_1nn_weights`.
 ///
@@ -133,17 +175,17 @@ pub fn classifier_1nn<T: Data<T> + Clone + Copy>(knowledge: &Vec<T>, exam: &Vec<
 /// * `n_attrs` - Number of attributes of the data.
 /// * `discarding_low_weights` - Boolean value, if true all weights under 0.2 are used as 0.0.
 ///
-/// **Note**: Doesn't return anything just print the result of each test.
+/// **Note**: Returns an instance of Results
 pub fn relief<T: Data<T> + Clone + Copy>(
     knowledge: &Vec<T>,
     exam: &Vec<T>,
     n_attrs: usize,
     discard_low_weights: bool,
-) -> Result<(), Box<std::error::Error>> {
-    // NOTE For each partition
+) -> Results {
+    // NOTE Initialize vector of weights.
     let mut weights: Vec<f32> = vec![0.0; n_attrs];
 
-    // NOTE Greedy
+    // NOTE Start Greedy loop
     for known in knowledge.iter() {
         let mut enemy_distance = std::f32::MAX;
         let mut friend_distance = std::f32::MAX;
@@ -151,7 +193,7 @@ pub fn relief<T: Data<T> + Clone + Copy>(
         let mut enemy_index = 0;
 
         for (index, candidate) in knowledge.iter().enumerate() {
-            //NOTE Skip if cantidate == known
+            // NOTE Skip if cantidate == known
             if candidate.get_id() != known.get_id() {
                 // NOTE Pre-calculate distance
                 let dist = known.euclidean_distance(candidate);
@@ -179,55 +221,84 @@ pub fn relief<T: Data<T> + Clone + Copy>(
             weights[attr] += (known.get_attr(attr) - enemy.get_attr(attr)).abs()
                 - (known.get_attr(attr) - ally.get_attr(attr)).abs();
         }
-    } // NOTE END Greedy
-
-    let mut highest_weight: f32 = 0.0;
-    for attr in 0..n_attrs {
-        // NOTE Find maximal element for future normalizing.
-        if weights[attr] > highest_weight {
-            highest_weight = weights[attr];
-        }
-        // NOTE Truncate negative values as 0.
-        if weights[attr] < 0.0 {
-            weights[attr] = 0.0;
-        }
     }
-
-    let mut reduction: f32 = 0.0;
-    // NOTE Normalize weights
-    for attr in 0..n_attrs {
-        weights[attr] = weights[attr] / highest_weight;
-        if weights[attr] < 0.2 {
-            reduction += 1.0;
-        }
-    }
-
-    let correct = classifier_1nn_weights(&knowledge, &exam, &weights, discard_low_weights);
-
-    reduction = 100.0 * reduction / n_attrs as f32;
-
-    let f = 0.5 * reduction + 0.5 * (100.0 * correct as f32 / exam.len() as f32);
-
-    if discard_low_weights {
-        println!(
-            "\t\tRelief discarding weights under 0.2: \n\t\t\tReduction rate: {} \n\t\t\tSuccess percentage: {}/{} = {}\n\t\t\tEvaluation function: {}",
-            reduction,
-            correct,
-            exam.len(),
-            100.0 * correct as f32 / exam.len() as f32,
-            f);
-    } else {
-        println!(
-            "\t\tRelief without discarding weights under 0.2: \n\t\t\tReduction rate: {} \n\t\t\tSuccess percentage: {}/{} = {}\n\t\t\tEvaluation function: {}",
-            reduction,
-            correct,
-            exam.len(),
-            100.0 * correct as f32 / exam.len() as f32,
-            f);
-    }
-
-    Ok(())
+    // NOTE END Greedy
+    normalize_and_truncate_weights(&mut weights);
+    return classifier_1nn_weights(&knowledge, &exam, &weights, discard_low_weights);
 }
+
+pub fn mutate_weights(weights: &mut Vec<f32>, desv: f64, index_to_mutate: usize) {
+    let normal = Normal::new(0.0, desv);
+    let mut rng = thread_rng();
+    weights[index_to_mutate] += normal.sample(&mut rng) as f32;
+}
+
+/// Prepares weights using a local search algorithm, so they are used in `classifier_1nn_weights`.
+///
+/// # Arguments
+/// * `knowledge` - Items whose class is known.
+/// * `exam` - Items whose class is unknown.
+/// * `n_attrs` - Number of attributes of the data.
+/// * `discarding_low_weights` - Boolean value, if true all weights under 0.2 are used as 0.0.
+///
+/// **Note**: Returns an instance of Results.
+pub fn local_search<T: Data<T> + Clone + Copy>(
+    knowledge: &Vec<T>,
+    exam: &Vec<T>,
+    n_attrs: usize,
+    discard_low_weights: bool,
+) -> Results {
+    let uniform = Uniform::new(0.0, 1.0);
+    let mut weights: Vec<f32> = vec![0.0; n_attrs];
+    let mut rng = thread_rng();
+    // NOTE Initialize weights using uniform distribution.
+    for attr in 0..n_attrs {
+        weights[attr] += uniform.sample(&mut rng);
+    }
+    // NOTE Initialize vector of index
+    let mut index_vec: Vec<usize> = Vec::new();
+    let mut index_to_mute: usize = refresh_index_vec(&mut index_vec, n_attrs);
+
+    let mut result = classifier_1nn_weights(&knowledge, &exam, &weights, discard_low_weights);
+
+    let max_neighbours = 20 * n_attrs;
+    let mut n_neighbours_generated = 0;
+    for _ in 0..15000 {
+        let mut muted_weights = weights.clone();
+        mutate_weights(&mut muted_weights, 0.2, index_to_mute);
+
+        normalize_and_truncate_weights(&mut muted_weights);
+
+        let muted_result =
+            classifier_1nn_weights(&knowledge, &exam, &muted_weights, discard_low_weights);
+
+        //NOTE if muted_weights is better
+        if muted_result.evaluation_function() > result.evaluation_function() {
+            // NOTE Reset neighbours count.
+            n_neighbours_generated = 0;
+
+            // NOTE Save new best results.
+            weights = muted_weights;
+            result = muted_result;
+
+            index_to_mute = refresh_index_vec(&mut index_vec, n_attrs);
+        } else {
+            n_neighbours_generated += 1;
+            if n_neighbours_generated == max_neighbours {
+                break;
+            }
+
+            if index_vec.is_empty() {
+                index_to_mute = refresh_index_vec(&mut index_vec, n_attrs);
+            } else {
+                index_to_mute = index_vec[0];
+            }
+        }
+    }
+
+    return result;
+}
+
 ///  Using the csv in `path`, prepares everything to call the different classifiers.
 ///
 /// # Arguments
@@ -242,7 +313,7 @@ pub fn run<T: Data<T> + Clone + Copy>(
     n_attrs: usize,
     folds: usize,
 ) -> Result<(), Box<std::error::Error>> {
-    //NOTE Read CSV
+    // NOTE Read CSV
     let mut csv_reader = csv::Reader::from_path(path).expect("Error reading csv file");
     let mut data: Vec<T> = Vec::new();
 
@@ -253,7 +324,7 @@ pub fn run<T: Data<T> + Clone + Copy>(
         let record = result?;
         let mut counter = 0;
         for field in record.iter() {
-            // NOTE CSV structure: id , ... attributes ... , class
+            // NOTE CSV structure: attributes... ,class
             if counter != n_attrs {
                 aux_record.set_attr(counter, field.parse::<f32>().unwrap());
             } else {
@@ -281,15 +352,26 @@ pub fn run<T: Data<T> + Clone + Copy>(
         let exam = data[i].clone();
 
         let mut now = Instant::now();
-        classifier_1nn(&knowledge, &exam);
+        let nn_result = classifier_1nn(&knowledge, &exam);
+        println!("\t\t1-NN results: \n{}", nn_result);
         println!("\t\t Time elapsed: {} ms.\n", now.elapsed().as_millis());
 
         now = Instant::now();
-        relief(&knowledge, &exam, n_attrs, true)?;
+        let relief_result = relief(&knowledge, &exam, n_attrs, true);
+        println!("\t\tRelief (discarding low weights) \n{}", relief_result);
         println!("\t\t Time Elapsed: {} ms.\n", now.elapsed().as_millis());
 
         now = Instant::now();
-        relief(&knowledge, &exam, n_attrs, false)?;
+        let relief_result2 = relief(&knowledge, &exam, n_attrs, false);
+        println!(
+            "\t\tRelief (not discarding low weights) \n{}",
+            relief_result2
+        );
+        println!("\t\t Time elapsed: {} ms.\n", now.elapsed().as_millis());
+
+        now = Instant::now();
+        let ls_result = local_search(&knowledge, &exam, n_attrs, false);
+        println!("\t\tLocal Search results: \n{}", ls_result);
         println!("\t\t Time elapsed: {} ms.\n", now.elapsed().as_millis());
     }
 
@@ -304,15 +386,15 @@ fn main() {
         std::process::exit(1);
     }
 
-    println!("## Results for Colposcopy.\n");
-    if let Err(err) = run::<Colposcopy>(String::from("data/colposcopy.csv"), 62, 5) {
-        println!("Error running Texture: {}", err);
-        std::process::exit(1);
-    }
+    // println!("## Results for Colposcopy.\n");
+    // if let Err(err) = run::<Colposcopy>(String::from("data/colposcopy.csv"), 62, 5) {
+    //     println!("Error running Texture: {}", err);
+    //     std::process::exit(1);
+    // }
 
-    println!("## Results for Ionosphere.\n");
-    if let Err(err) = run::<Ionosphere>(String::from("data/ionosphere.csv"), 34, 5) {
-        println!("Error running Texture: {}", err);
-        std::process::exit(1);
-    }
+    // println!("## Results for Ionosphere.\n");
+    // if let Err(err) = run::<Ionosphere>(String::from("data/ionosphere.csv"), 34, 5) {
+    //     println!("Error running Texture: {}", err);
+    //     std::process::exit(1);
+    // }
 }
--- src/structs.rs
+++ src/structs.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 /// Trait for CSV data
 ///
 /// **Note**: The struct implementing this trait must also implement `Copy` and `Clone`.
@@ -12,44 +13,92 @@ pub trait Data<T> {
     fn euclidean_distance(&self, other: &T) -> f32;
 }
 
+pub struct Results {
+    pub low_weights: f32,
+    pub correct_answers: f32,
+    pub exam_len: f32,
+    pub n_attrs: f32,
+}
+
+impl Results {
+    pub fn new(
+        _low_weights: u32,
+        _correct_answers: u32,
+        _exam_len: usize,
+        _n_attrs: usize,
+    ) -> Results {
+        Results {
+            low_weights: _low_weights as f32,
+            correct_answers: _correct_answers as f32,
+            exam_len: _exam_len as f32,
+            n_attrs: _n_attrs as f32,
+        }
+    }
+    pub fn reduction_rate(&self) -> f32 {
+        if self.n_attrs == 0.0 {
+            return 0.0;
+        }
+        return self.low_weights / self.n_attrs;
+    }
+    pub fn success_percentage(&self) -> f32 {
+        return self.correct_answers / self.exam_len;
+    }
+    pub fn evaluation_function(&self) -> f32 {
+        return 0.5 * self.reduction_rate() + 0.5 * self.success_percentage();
+    }
+}
+
+impl fmt::Display for Results {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f, "\t\t\tReduction rate: {} \n\t\t\tSuccess percentage: {}/{} = {}\n\t\t\tEvaluation function: {}",
+           self.reduction_rate(),
+           self.correct_answers,
+           self.exam_len,
+           self.success_percentage(),
+           self.evaluation_function(),
+        )
+    }
+}
+
 #[derive(Copy, Clone)]
 pub struct Texture {
-    pub _id: i32,
-    pub _attrs: [f32; 40],
-    pub _class: i32,
+    id: i32,
+    attrs: [f32; 40],
+    class: i32,
 }
 
 impl Data<Texture> for Texture {
     fn new() -> Texture {
         Texture {
-            _id: -1,
-            _attrs: [0.0; 40],
-            _class: -1,
+            id: -1,
+            attrs: [0.0; 40],
+            class: -1,
         }
     }
     fn get_id(&self) -> i32 {
-        return self._id;
+        return self.id;
     }
     fn get_attr(&self, index: usize) -> f32 {
-        return self._attrs[index];
+        return self.attrs[index];
     }
     fn get_class(&self) -> i32 {
-        return self._class;
+        return self.class;
     }
     fn set_id(&mut self, value: i32) -> () {
-        self._id = value;
+        self.id = value;
     }
     fn set_attr(&mut self, index: usize, value: f32) -> () {
-        self._attrs[index] = value;;
+        self.attrs[index] = value;;
     }
     fn set_class(&mut self, value: i32) -> () {
-        self._class = value;
+        self.class = value;
     }
     fn euclidean_distance(&self, other: &Texture) -> f32 {
         let mut sum = 0.0;
         for index in 0..40 {
-            sum += (self._attrs[index] - other._attrs[index])
-                * (self._attrs[index] - other._attrs[index])
+            sum +=
+                (self.attrs[index] - other.attrs[index]) * (self.attrs[index] - other.attrs[index])
         }
         return sum.sqrt();
     }
@@ -57,42 +106,42 @@ impl Data<Texture> for Texture {
 
 #[derive(Copy, Clone)]
 pub struct Colposcopy {
-    pub _id: i32,
-    pub _attrs: [f32; 62],
-    pub _class: i32,
+    id: i32,
+    attrs: [f32; 62],
+    class: i32,
 }
 
 impl Data<Colposcopy> for Colposcopy {
     fn new() -> Colposcopy {
         Colposcopy {
-            _id: -1,
-            _attrs: [0.0; 62],
-            _class: -1,
+            id: -1,
+            attrs: [0.0; 62],
+            class: -1,
         }
     }
     fn get_id(&self) -> i32 {
-        return self._id;
+        return self.id;
     }
     fn get_attr(&self, index: usize) -> f32 {
-        return self._attrs[index];
+        return self.attrs[index];
     }
     fn get_class(&self) -> i32 {
-        return self._class;
+        return self.class;
     }
     fn set_id(&mut self, value: i32) -> () {
-        self._id = value;
+        self.id = value;
     }
     fn set_attr(&mut self, index: usize, value: f32) -> () {
-        self._attrs[index] = value;;
+        self.attrs[index] = value;
     }
     fn set_class(&mut self, value: i32) -> () {
-        self._class = value;
+        self.class = value;
     }
     fn euclidean_distance(&self, other: &Colposcopy) -> f32 {
         let mut sum = 0.0;
         for index in 0..62 {
-            sum += (self._attrs[index] - other._attrs[index])
-                * (self._attrs[index] - other._attrs[index])
+            sum +=
+                (self.attrs[index] - other.attrs[index]) * (self.attrs[index] - other.attrs[index])
         }
         return sum.sqrt();
     }
@@ -100,42 +149,42 @@ impl Data<Colposcopy> for Colposcopy {
 
 #[derive(Copy, Clone)]
 pub struct Ionosphere {
-    pub _id: i32,
-    pub _attrs: [f32; 34],
-    pub _class: i32,
+    id: i32,
+    attrs: [f32; 34],
+    class: i32,
 }
 
 impl Data<Ionosphere> for Ionosphere {
     fn new() -> Ionosphere {
         Ionosphere {
-            _id: -1,
-            _attrs: [0.0; 34],
-            _class: -1,
+            id: -1,
+            attrs: [0.0; 34],
+            class: -1,
         }
     }
     fn get_id(&self) -> i32 {
-        return self._id;
+        return self.id;
     }
     fn get_attr(&self, index: usize) -> f32 {
-        return self._attrs[index];
+        return self.attrs[index];
     }
     fn get_class(&self) -> i32 {
-        return self._class;
+        return self.class;
     }
     fn set_id(&mut self, value: i32) -> () {
-        self._id = value;
+        self.id = value;
     }
     fn set_attr(&mut self, index: usize, value: f32) -> () {
-        self._attrs[index] = value;;
+        self.attrs[index] = value;;
     }
     fn set_class(&mut self, value: i32) -> () {
-        self._class = value;
+        self.class = value;
     }
     fn euclidean_distance(&self, other: &Ionosphere) -> f32 {
         let mut sum = 0.0;
         for index in 0..34 {
-            sum += (self._attrs[index] - other._attrs[index])
-                * (self._attrs[index] - other._attrs[index])
+            sum +=
+                (self.attrs[index] - other.attrs[index]) * (self.attrs[index] - other.attrs[index])
         }
         return sum.sqrt();
     }
